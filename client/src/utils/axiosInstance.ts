cc9e5a35ce2ff6128ee18501282389de34b32daf
SolomonAvraham <Selmon_a@hotmail.com>
2024-09-11 21:08:30 +0300

    update baseUrl axios

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_SERVER_URL
+    ? "http://13.60.96.4:9001"
     : process.env.NEXT_PUBLIC_LOCAL_SERVER_URL;
 
 const axiosInstance = axios.create({
