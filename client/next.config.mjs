a8c3cfd3582e5b87a97fffe884a9c07c3db740c9
SolomonAvraham <Selmon_a@hotmail.com>
2024-09-15 21:59:28 +0300

    debugin nextjs middleware

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // All API requests
+        destination: "https://adgdbgbbbfcgbdsbdg.online/:path*", // Proxy to backend
+      },
+    ];
+  },
+  experimental: {
+    // Disable Turbopack
+    turbopack: false,
+  },
+};
 
 export default nextConfig;
