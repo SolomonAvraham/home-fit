6bb1770f8a604671b64ac078a7339d59039da5f6
SolomonAvraham <Selmon_a@hotmail.com>
2024-09-15 16:29:46 +0300

    debugin nextjs middleware

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -37,11 +37,15 @@ class UserController {
         password
       );
 
+      const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
         path: "/",
+        expires: expires,
+        domain: ".homefit-pro.vercel.app",
       });
 
       res.status(200).json({ id, name, role });
