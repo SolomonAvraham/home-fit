f9f6da0be3bff466fd231406189d73b45c41327a
SolomonAvraham <Selmon_a@hotmail.com>
2024-09-17 22:39:00 +0300

    adding token to axios instance

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const baseURL =
   process.env.NODE_ENV === "production"
@@ -12,5 +13,20 @@ const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
